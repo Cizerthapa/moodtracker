f8ef883f7dacc02465ec23ed9a177fbb417d4824
Cizerthapa <cizerthapa@gmail.com>
2026-05-02 23:07:42 +0545

    feat: add UIStateWrapper widget to handle global loading overlays, offline banners, and error snackbars

--- lib/core/widgets/ui_state_wrapper.dart
+++ lib/core/widgets/ui_state_wrapper.dart
@@ -44,40 +44,45 @@ class UIStateWrapper extends StatelessWidget {
                 top: 0,
                 left: 0,
                 right: 0,
-                child: Material(
-                  color: Colors.transparent,
-                  child: Container(
-                    padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).padding.top + 8.h,
-                      bottom: 8.h,
-                    ),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFC4635A), // Soft warm red
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 10,
-                          offset: const Offset(0, 4),
+                child:
+                    Material(
+                      color: Colors.transparent,
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).padding.top + 8.h,
+                          bottom: 8.h,
                         ),
-                      ],
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.wifi_off_rounded, color: Colors.white, size: 16.r),
-                        SizedBox(width: 8.w),
-                        Text(
-                          "No Internet Connection",
-                          style: GoogleFonts.outfit(
-                            color: Colors.white,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFC4635A), // Soft warm red
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 10,
+                              offset: const Offset(0, 4),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ).animate().slideY(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.wifi_off_rounded,
+                              color: Colors.white,
+                              size: 16.r,
+                            ),
+                            SizedBox(width: 8.w),
+                            Text(
+                              "No Internet Connection",
+                              style: GoogleFonts.outfit(
+                                color: Colors.white,
+                                fontSize: 12.sp,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ).animate().slideY(
                       begin: -1,
                       end: 0,
                       duration: 400.ms,
@@ -90,23 +95,24 @@ class UIStateWrapper extends StatelessWidget {
               Container(
                 color: Colors.black.withOpacity(0.3),
                 child: Center(
-                  child: Container(
-                    padding: EdgeInsets.all(24.r),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20.r),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 20,
+                  child:
+                      Container(
+                        padding: EdgeInsets.all(24.r),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.r),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 20,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: CircularProgressIndicator(
-                      color: AppColors.roseDeep,
-                      strokeWidth: 3,
-                    ),
-                  ).animate().scale(
+                        child: CircularProgressIndicator(
+                          color: AppColors.roseDeep,
+                          strokeWidth: 3,
+                        ),
+                      ).animate().scale(
                         duration: 300.ms,
                         curve: Curves.easeOutBack,
                       ),
